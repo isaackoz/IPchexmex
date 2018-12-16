c36ee6b0fcfe32456b4453e2c69be4e809402051
DESKTOP-4CI3BRI\Isaac <isaackoz130@gmail.com>
2018-12-15 23:20:35 -0800

    only blocks Quebec instead of the entire Canada

--- vpn checker/content.js
+++ vpn checker/content.js
@@ -1,5 +1,5 @@
 var $ = window.jQuery;
-var invalid_country_codes = ["US","CA","CU","IR","KP","SD"];
+var invalid_country_codes = ["US","CU","IR","KP","SD"];
 var iccLength = Object.keys(invalid_country_codes).length;
 
 $(document).ready(function($) {	
@@ -11,11 +11,11 @@ function secured() {
 	$.getJSON('https://json.geoiplookup.io/', {"_": new Date().getTime()}, function(data) {
 		console.log('IPchexmex: checking if secured');
 		for(var i = 0; i < iccLength; i++) {
-			if(data.country_code == invalid_country_codes[i]) {
+			if(data.country_code == invalid_country_codes[i] || data.region == "Quebec") {
 				alert('WARNING! Your IP is in country prohibited from holding positions or entering into contracts at BitMEX! Use a VPN before continuing. Current location: ' + data.country_name);
 				console.log('IPchexmex: not secured');
 				return false
-			}
+			}	
 			else {
 				console.log('IPchexmex: secured');
 				return true
