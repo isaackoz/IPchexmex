d008cbc0d76f73a6d19c1e5741c08bd99d78896e
DESKTOP-4CI3BRI\Isaac <isaackoz130@gmail.com>
2018-12-15 18:41:30 -0800

    inital commit

---
+++ vpn checker/content.js
@@ -0,0 +1,27 @@
+var $ = window.jQuery;
+var invalid_country_codes = ["US","CA","CU","IR","KP","SD"];
+var iccLength = Object.keys(invalid_country_codes).length;
+
+$(document).ready(function($) {	
+	secured();
+	setInterval(function() {secured()}, 10000);
+});
+
+function secured() {
+	$.getJSON('https://json.geoiplookup.io/', {"_": new Date().getTime()}, function(data) {
+		console.log('IPchexmex: checking if secured');
+		for(var i = 0; i < iccLength; i++) {
+			if(data.country_code == invalid_country_codes[i]) {
+				alert('WARNING! Your IP is in country prohibited from holding positions or entering into contracts at BitMEX! Use a VPN before continuing. Current location: ' + data.country_name);
+				console.log('IPchexmex: not secured');
+				return false
+			}
+			else {
+				console.log('IPchexmex: secured');
+				return true
+			}
+		}
+	});
+}
+
+
